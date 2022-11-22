3131a556f41a39f2ffa7986c335a30c87c894014
sswdr <771020712@qq.com>
2022-11-22 17:00:59 +0800

    code

---
+++ src/main/java/com/ssw/MainApplication.java
@@ -0,0 +1,15 @@
+package com.ssw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ssw
+ * @date 2022/11/22 10:49
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
